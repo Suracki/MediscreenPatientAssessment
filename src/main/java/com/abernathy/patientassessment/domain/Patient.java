af5e101e3a9f8d22dde97bbd0aa5ec1f255b7df6
Suracki <simon.linford@gmail.com>
2022-06-07 14:48:38 +0200

    add_classes_to_represent_model (Generate Diabetes Assessment)
    - added classes to hold Patient and PatientNote objects from database retrieval
    - created PatientAssessment to hold a Patient, their notes, and their risk
    - created Risk enum to hold risk assessment result
    
    Used an enum to hold the assessment result to ensure the value always matched one of the set terms, while also allowing for additional granularity to be easily added in future.
    In order to allow output to match requested format a custom method was added to convert this enum to a String (as the desired format includes spaces). Possible there may be a cleaner way to implement this later.
    
    https://trello.com/c/cedtNn3j/7-generate-diabetes-assessment

---
+++ src/main/java/com/abernathy/patientassessment/domain/Patient.java
@@ -0,0 +1,95 @@
+package com.abernathy.patientassessment.domain;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+public class Patient {
+    private int patientId;
+    private String familyName;
+    private String givenName;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date dob;
+    private String sex;
+    private String address;
+    private String phone;
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public void setId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public int getId() {
+        return patientId;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "{\"patient\":{\"patientId\": \"" + patientId +
+                "\", \"familyName\": \"" + familyName +
+                "\", \"givenName\": \"" + givenName +
+                "\", \"dob\": \"" + dob +
+                "\", \"sex\": \"" + sex +
+                "\", \"address\": \"" + address +
+                "\", \"phone\": \"" + phone +"\"}}";
+    }
+
+    public void setId(Integer id) {
+        this.patientId = id;
+    }
+}
