4023344aaa128c170af11f16da7a5a213c872e81
Suracki <simon.linford@gmail.com>
2022-06-09 15:07:53 +0200

    add_endpoints_for_ui (Generate Diabetes Assessment)
    - Added endpoint to PatientAssessmentController to serve UI request
    - Added method to PatientAssessmentService to perform requests & return page
    - Added Thymeleaf to pom & added blank html templates for testing
    
    https://trello.com/c/cedtNn3j/7-generate-diabetes-assessment

--- src/main/java/com/abernathy/patientassessment/service/PatientAssessmentService.java
+++ src/main/java/com/abernathy/patientassessment/service/PatientAssessmentService.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
 import java.util.List;
 
@@ -40,6 +41,21 @@ public class PatientAssessmentService {
         catch (NoNotesFoundException f) {
             return new ResponseEntity<String>("Patient " + patId + " has no notes.", new HttpHeaders(), HttpStatus.NOT_FOUND);
         }
+    }
+
+    public String patientRiskAssessmentWebRequest(int id, Model model) {
+        try {
+            PatientAssessment patientAssessment = assessPatientRisk(id);
+            model.addAttribute("patientAssessment", patientAssessment);
+            return "/assessment/view";
+        }
+        catch (PatientNotFoundException e) {
+            return "/patnotfound";
+        }
+        catch (NoNotesFoundException f) {
+            return "/patnonotes";
+        }
+
     }
 
     public PatientAssessment assessPatientRisk(int patId) throws PatientNotFoundException, NoNotesFoundException {
@@ -146,5 +162,4 @@ public class PatientAssessmentService {
         }
         return triggerCount.getTriggerCount();
     }
-
 }
